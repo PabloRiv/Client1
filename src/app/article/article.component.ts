d8a18547cff8d5af239159616b6ba2100379e6a3
Perry Biava <perry.biava@biavaInteractive.com>
2017-05-06 15:40:09 -0400

    second try at commit all files from angular-reddit

---
+++ src/app/article/article.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, HostBinding, Input } from '@angular/core';
+import {Article} from './article.model';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css']
+})
+export class ArticleComponent implements OnInit {
+@Input() article: Article
+@HostBinding('attr.class') cssClass = 'row'
+
+articles: Article[];
+
+  constructor() { 
+   
+  }
+
+  ngOnInit() {
+  }
+
+  voteUp(): boolean {
+    this.article.voteUp();
+    return false;
+  }
+
+   voteDown(): boolean {
+    this.article.voteDown();
+    return false;
+  }
+}
