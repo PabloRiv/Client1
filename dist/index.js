8b66d0d5d08f69fdd2151f7a67618d14ecef20a6
Perry Biava <perry.biava@biavaInteractive.com>
2017-05-18 11:23:08 -0400

    changed express server config to serve up dist folder in index.js again

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,7 @@ var express = require('express'),
     fs = require('fs');
 
 var app = express();
-var staticRoot = __dirname + '/dist';
+var staticRoot = __dirname + '/dist/';
 
 app.set('port', (process.env.PORT || 3000));
 
